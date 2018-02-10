b4300da34a968683228439ecd129a38dc217ff94
River Kanies <river.kanies@gmail.com>
2018-02-10 14:00:59 -0500

    copy level string to clipboard, pixel styles

--- imports/ui/Map.js
+++ imports/ui/Map.js
@@ -7,21 +7,30 @@ export default class Map extends Component {
 
     this.selectMap = this.selectMap.bind(this)
     this.rename = this.rename.bind(this)
+    this.copy = this.copy.bind(this)
   }
   render() {
+    const {map} = this.props
     let style = {margin: '10px 0'}
     if (this.props.selected) style.border = '2px solid blue'
     return (
       <li onClick={this.selectMap} >
-        <input value={this.props.map.text} onChange={this.rename} style={style}/>
+        <input value={map.text} onChange={this.rename} style={style}/>
+        <button onClick={this.copy}>Copy</button>
+        <input id={`copy-${map._id}`} style={{position: 'absolute', marginLeft: '110%'}} value={JSON.stringify(map.level)} />
       </li>
     );
   }
   selectMap(){
-    console.log('map', this.props.mapIndex)
     this.props.ss({selectedMap: this.props.mapIndex})
   }
   rename(e) {
     Meteor.call('maps.update', {...this.props.map, text: e.target.value})
+  }
+  copy() {
+    const el = document.getElementById(`copy-${this.props.map._id}`)
+    if (!el) return
+    el.select()
+    document.execCommand("Copy")
   }
 }
